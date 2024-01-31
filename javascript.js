ae8f6a5d4f79ceec673b6516e9a83042a1ef9797
James Boyd <jamesboyd2011@live.com>
2024-01-31 18:15:28 -0500

    Updated work

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
 const API_URL =
-  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2311-FSA-ET-WEB-PT-SF/events";
+  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2311-FSA-ET-WEB-PT-SF";
 const state = {
   rawPartyData: [],
+  rawRsvps: {},
+  rawGuests: {},
   htmlPartyCards: [],
+  htmlPartyDetails: {},
 };
 const partyList = document.createElement("div");
 partyList.id = "PartyList";
@@ -13,7 +16,7 @@ class partyForm {
     const card = document.createElement("card");
     const Form = document.createElement("form");
     Form.className = "form";
-    this.#initInput(Form,"New Party Form","","hidden")
+    this.#initInput(Form, "New Party Form", "", "hidden");
     this.#initInput(Form, "What should we call this party?", "name");
     this.#initInput(Form, "Where is this party?", "location");
     this.#initTextarea(Form, "Why are we partying?", "description");
@@ -70,11 +73,35 @@ getParties();
 
 async function getParties() {
   try {
-    const response = await fetch(API_URL);
-    if (response.ok) {
-      const data = await response.json();
-      state.rawPartyData = data.data.sort((a, b) => (a.date < b.date ? -1 : 1));
+    const eventsResponse = await fetch(`${API_URL}/events`);
+    const rsvpsResponse = await fetch(`${API_URL}/rsvps`);
+    const guestsResponse = await fetch(`${API_URL}/guests`);
+
+    if (eventsResponse.ok) {
+      const eventData = await eventsResponse.json();
+      state.rawPartyData = eventData.data.sort((a, b) =>
+        a.date < b.date ? -1 : 1
+      );
     }
+
+    if (rsvpsResponse.ok && guestsResponse.ok) {
+      const rsvpsData = await rsvpsResponse.json();
+      const guestsData = await guestsResponse.json();
+
+      state.rawRsvps = rsvpsData.data.reduce((obj, { eventId, guestId }) => {
+        !obj[eventId] ? (obj[eventId] = [guestId]) : obj[eventId].push(guestId);
+        return obj;
+      }, {});
+
+      state.rawGuests = guestsData.data.reduce(
+        (obj, { name, phone, id, email }) => {
+          !obj[id] ? (obj[id] = { name, phone, email }) : null;
+          return obj;
+        },
+        {}
+      );
+    }
+
     buildPartyCards(state);
   } catch (error) {
     console.log(error);
@@ -87,12 +114,43 @@ function buildPartyCards({ rawPartyData }) {
       let card = document.createElement("card");
 
       card.setAttribute("partyID", id);
-      card.innerHTML = `
+      let frontPage = document.createElement("page");
+      card.append(frontPage);
+      frontPage.className = "frontPage";
+      frontPage.innerHTML = `
       <partyTitle>${name}</partyTitle>
       <partyLocation>${location}</partyLocation>
       <partyDesc>${description}</partyDesc>
-      <partyDate>${new Date(date).toLocaleString().replace(","," @")}</partyDate>
+      <partyDate>${new Date(date)
+        .toLocaleString()
+        .replace(",", " @")}</partyDate>
       `;
+
+      if (state.rawRsvps[id]) {
+        const showAttendeesButton = document.createElement("partyRSVP");
+        showAttendeesButton.innerHTML = "Show Attendees";
+
+        frontPage.append(showAttendeesButton);
+        const guestList = document.createElement("guestPage");
+        state.rawRsvps[id].forEach((guestId) => {
+          !state.htmlPartyDetails[id]
+            ? (state.htmlPartyDetails[id] = [])
+            : state.htmlPartyDetails[id];
+          let li = document.createElement("li");
+          let guest = state.rawGuests[guestId];
+          li.innerHTML = `${guest.name}`;
+          state.htmlPartyDetails[id].push(li);
+        });
+        card.append(guestList);
+        guestList.replaceChildren(...state.htmlPartyDetails[id]);
+
+        showAttendeesButton.addEventListener("click", (evt) => {
+          card.classList.toggle("showAttendees");
+          card.classList.contains("showAttendees")
+            ? (showAttendeesButton.innerText = "Hide Attendees")
+            : (showAttendeesButton.innerText = "Show Attendees");
+        });
+      }
 
       let deletePartyButton = document.createElement("deleteParty");
       deletePartyButton.innerText = "X";
@@ -119,7 +177,7 @@ async function addParty({ name, location, date, description }) {
       location: location.value,
     };
     if (confirm("Are you sure you want to add this party?")) {
-      const response = await fetch(API_URL, {
+      const response = await fetch(`${API_URL}/events`, {
         headers: {
           accept: "application/json",
           "content-type": "application/json",
@@ -142,7 +200,7 @@ async function addParty({ name, location, date, description }) {
 async function removeParty(id) {
   try {
     if (confirm("Are you sure you want to remove this party?")) {
-      const response = await fetch(`${API_URL}/${id}`, {
+      const response = await fetch(`${API_URL}/events/${id}`, {
         method: "DELETE",
       });
 
